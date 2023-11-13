9766e5bfdf7a9de54bf1b73f70f543bd47cd157e
Juan Sebastian <jsebas2426@gmail.com>
2023-11-13 08:23:13 -0500

    feat: build code

--- dist/index.js
+++ dist/index.js
@@ -1,56 +1,90 @@
 #! /usr/bin/env node
-'use strict'
-const __create = Object.create
-const __defProp = Object.defineProperty
-const __getOwnPropDesc = Object.getOwnPropertyDescriptor
-const __getOwnPropNames = Object.getOwnPropertyNames
-const __getProtoOf = Object.getPrototypeOf
-const __hasOwnProp = Object.prototype.hasOwnProperty
-const __copyProps = (to, from, except, desc) => {
-  if (from && typeof from === 'object' || typeof from === 'function') {
-    for (const key of __getOwnPropNames(from)) {
-      if (!__hasOwnProp.call(to, key) && key !== except) { __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable }) }
-    }
+"use strict";
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
   }
-  return to
-}
-const __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  return to;
+};
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
   // If the importer is in node compatibility mode or this is not an ESM
   // file that has been converted to a CommonJS file using a Babel-
   // compatible transform (i.e. "__esModule" has not been set), then set
   // "default" to the CommonJS "module.exports" for node compatibility.
-  isNodeMode || !mod || !mod.__esModule ? __defProp(target, 'default', { value: mod, enumerable: true }) : target,
+  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
   mod
-))
+));
 
 // src/index.ts
-const import_yargs = __toESM(require('yargs'))
-const import_helpers = require('yargs/helpers')
+var import_yargs = __toESM(require("yargs/yargs"));
+var import_helpers = require("yargs/helpers");
 
 // src/createEnvFile.ts
-const import_fs = __toESM(require('fs'))
-const createEnvFile = (envVariables, outputPath = '.env') => {
-  const envContent = Object.entries(envVariables).map(([key, value]) => `${key}=${value}`).join('\n')
-  import_fs.default.writeFileSync(outputPath, envContent, 'utf-8')
-}
+var import_fs = __toESM(require("fs"));
+var createEnvFile = (envVariables, outputPath = ".env") => {
+  let existingEnvVariables = {};
+  if (import_fs.default.existsSync(outputPath)) {
+    const existingEnvContent = import_fs.default.readFileSync(outputPath, "utf-8");
+    existingEnvVariables = existingEnvContent.split("\n").filter((line) => line.trim() !== "").reduce((acc, line) => {
+      const [name, value = ""] = line.split("=");
+      acc[name] = value;
+      return acc;
+    }, {});
+  }
+  const newEnvVariables = envVariables.filter((env) => existingEnvVariables[env] === void 0);
+  const envContent = Object.assign({}, existingEnvVariables, ...newEnvVariables.map((env) => ({ [env]: "" })));
+  const envContentString = Object.entries(envContent).map(([name, value]) => `${name}=${value}`).join("\n");
+  import_fs.default.writeFileSync(outputPath, envContentString, "utf-8");
+};
 
 // src/getEnvs.ts
-const import_glob = require('glob')
-const scansFiles = (directory2) => {
-  const result = []
-  const files = (0, import_glob.globSync)('**/*.{ts,js}', { ignore: ['node_modules/**', 'dist/**'] })
-  console.log(files)
-  return result
-}
-const getEnvs = (directory2) => {
-  const filesWithEnvs = scansFiles(directory2)
-  console.log(filesWithEnvs)
-}
+var import_fs2 = __toESM(require("fs"));
+var import_glob = require("glob");
+var import_path = __toESM(require("path"));
+var scansFiles = (directory2) => {
+  const result = [];
+  const files = (0, import_glob.globSync)("**/*.{ts,js}", { ignore: ["node_modules/**", "dist/**"] });
+  files.forEach((file) => {
+    const filePath = import_path.default.join(directory2, file);
+    const content = import_fs2.default.readFileSync(filePath, "utf-8");
+    if (content.includes("process.env")) {
+      result.push(filePath);
+    }
+  });
+  return result;
+};
+var getEnvs = (directory2) => {
+  const filesWithEnvs = scansFiles(directory2);
+  const envVariables = [];
+  filesWithEnvs.forEach((file) => {
+    const content = import_fs2.default.readFileSync(file, "utf-8");
+    const regex = /process\.env\.([A-Za-z_]+)/g;
+    let match;
+    while ((match = regex.exec(content)) !== null) {
+      envVariables.push(match[1]);
+    }
+  });
+  return envVariables;
+};
 
 // src/index.ts
-const argv = (0, import_yargs.default)((0, import_helpers.hideBin)(process.argv)).scriptName('extract-env').usage('Usage: $0').help().argv
-const directory = process.cwd()
-const envs = getEnvs(directory)
-createEnvFile({
-  'API-KEYYY': '123'
-})
+var argv = (0, import_yargs.default)((0, import_helpers.hideBin)(process.argv)).options({
+  f: {
+    type: "string",
+    default: ".env",
+    describe: "name of the file where the variables will be placed",
+    alias: "fileName"
+  }
+}).scriptName("extract-env").usage("Usage: $0").help().parseSync();
+var fileName = argv.fileName;
+var directory = process.cwd();
+var envs = getEnvs(directory);
+createEnvFile(envs, fileName);
--- dist/index.mjs
+++ dist/index.mjs
@@ -1,33 +1,67 @@
 #! /usr/bin/env node
 
 // src/index.ts
-import yargs from 'yargs'
-import { hideBin } from 'yargs/helpers'
+import yargs from "yargs/yargs";
+import { hideBin } from "yargs/helpers";
 
 // src/createEnvFile.ts
-import fs from 'fs'
+import fs from "fs";
+var createEnvFile = (envVariables, outputPath = ".env") => {
+  let existingEnvVariables = {};
+  if (fs.existsSync(outputPath)) {
+    const existingEnvContent = fs.readFileSync(outputPath, "utf-8");
+    existingEnvVariables = existingEnvContent.split("\n").filter((line) => line.trim() !== "").reduce((acc, line) => {
+      const [name, value = ""] = line.split("=");
+      acc[name] = value;
+      return acc;
+    }, {});
+  }
+  const newEnvVariables = envVariables.filter((env) => existingEnvVariables[env] === void 0);
+  const envContent = Object.assign({}, existingEnvVariables, ...newEnvVariables.map((env) => ({ [env]: "" })));
+  const envContentString = Object.entries(envContent).map(([name, value]) => `${name}=${value}`).join("\n");
+  fs.writeFileSync(outputPath, envContentString, "utf-8");
+};
 
 // src/getEnvs.ts
-import { globSync } from 'glob'
-const createEnvFile = (envVariables, outputPath = '.env') => {
-  const envContent = Object.entries(envVariables).map(([key, value]) => `${key}=${value}`).join('\n')
-  fs.writeFileSync(outputPath, envContent, 'utf-8')
-}
-const scansFiles = (directory2) => {
-  const result = []
-  const files = globSync('**/*.{ts,js}', { ignore: ['node_modules/**', 'dist/**'] })
-  console.log(files)
-  return result
-}
-const getEnvs = (directory2) => {
-  const filesWithEnvs = scansFiles(directory2)
-  console.log(filesWithEnvs)
-}
+import fs2 from "fs";
+import { globSync } from "glob";
+import path from "path";
+var scansFiles = (directory2) => {
+  const result = [];
+  const files = globSync("**/*.{ts,js}", { ignore: ["node_modules/**", "dist/**"] });
+  files.forEach((file) => {
+    const filePath = path.join(directory2, file);
+    const content = fs2.readFileSync(filePath, "utf-8");
+    if (content.includes("process.env")) {
+      result.push(filePath);
+    }
+  });
+  return result;
+};
+var getEnvs = (directory2) => {
+  const filesWithEnvs = scansFiles(directory2);
+  const envVariables = [];
+  filesWithEnvs.forEach((file) => {
+    const content = fs2.readFileSync(file, "utf-8");
+    const regex = /process\.env\.([A-Za-z_]+)/g;
+    let match;
+    while ((match = regex.exec(content)) !== null) {
+      envVariables.push(match[1]);
+    }
+  });
+  return envVariables;
+};
 
 // src/index.ts
-const argv = yargs(hideBin(process.argv)).scriptName('extract-env').usage('Usage: $0').help().argv
-const directory = process.cwd()
-const envs = getEnvs(directory)
-createEnvFile({
-  'API-KEYYY': '123'
-})
+var argv = yargs(hideBin(process.argv)).options({
+  f: {
+    type: "string",
+    default: ".env",
+    describe: "name of the file where the variables will be placed",
+    alias: "fileName"
+  }
+}).scriptName("extract-env").usage("Usage: $0").help().parseSync();
+var fileName = argv.fileName;
+var directory = process.cwd();
+var envs = getEnvs(directory);
+createEnvFile(envs, fileName);
