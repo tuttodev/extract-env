cec7f7fa20c24cf313eff81aff43d43d85e868fb
Juan Sebastian <jsebas2426@gmail.com>
2023-11-13 07:52:07 -0500

    feat: not overwriting existing variables

--- src/createEnvFile.ts
+++ src/createEnvFile.ts
@@ -1,9 +1,27 @@
 import fs from 'fs'
 
 export const createEnvFile = (envVariables: string[], outputPath = '.env'): void => {
-  const envContent = envVariables
-    .map((env) => `${env}=`)
+  let existingEnvVariables: Record<string, string> = {}
+
+  if (fs.existsSync(outputPath)) {
+    const existingEnvContent = fs.readFileSync(outputPath, 'utf-8')
+    existingEnvVariables = existingEnvContent
+      .split('\n')
+      .filter((line) => line.trim() !== '')
+      .reduce((acc: Record<string, string>, line) => {
+        const [name, value = ''] = line.split('=')
+        acc[name] = value
+        return acc
+      }, {})
+  }
+
+  const newEnvVariables = envVariables.filter((env) => existingEnvVariables[env] === undefined)
+
+  const envContent = Object.assign({}, existingEnvVariables, ...newEnvVariables.map((env) => ({ [env]: '' })))
+
+  const envContentString = Object.entries(envContent)
+    .map(([name, value]) => `${name}=${value as string}`)
     .join('\n')
 
-  fs.writeFileSync(outputPath, envContent, 'utf-8')
+  fs.writeFileSync(outputPath, envContentString, 'utf-8')
 }
